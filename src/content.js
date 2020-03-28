6ebcfd92db3abcc0fb1aae6c19e9b57daa4df207
Franco <wong9661@mylaurier.ca>
2020-03-28 16:23:47 -0400

    Added logic to take img meta data

--- src/content.js
+++ src/content.js
@@ -1,1 +1,16 @@
-alert('Coming from Franco Extension');
+const listOfImgs = document.querySelectorAll("img");
+
+let allImgs = [];
+let currImgMetaData = {};
+
+for(let img of listOfImgs){
+    currImgMetaData["src"] = img.src;
+    currImgMetaData["todaysDate"] = "";
+    currImgMetaData["alt"] = img.alt;
+    currImgMetaData["width"] = img.width;
+    currImgMetaData["height"] = img.height;
+    allImgs.push(currImgMetaData);
+}
+
+console.log(allImgs);
+// src, alt, todays date, width, height
