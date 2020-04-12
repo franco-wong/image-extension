f24d2f3be641a19707c44808c68b054fa99428e8
Naween M <Nmehanmal@hotmail.com>
2020-04-11 20:58:25 -0400

    Add supporting styling and logic for the sidebar component

--- src/components/ShadowDOMContainer.js
+++ src/components/ShadowDOMContainer.js
@@ -1,22 +1,24 @@
 import { fetchResourceString } from '../utility/helper';
 
+/**
+ * Base class meant for components to extend common functionality. Instances that will make use
+ * of the Shadow DOM and inject markup with styles.
+ */
 export default class ShadowDOMContainer {
   constructor(containerStyles) {
+    this.containerStyles = containerStyles
     this.hostNode = null;
     this.shadowRoot = null;
-    this.wrapper = null;
+    this.container = null;
     this.isShown = false;
-    this.containerStyles = containerStyles
   }
 
-  init() {
+  async init() {
     this.hostNode = document.createElement('div');
     this.shadowRoot = this.hostNode.attachShadow({ mode: 'open' });
-    this.injectMarkup();
-    this.injectStyles();
-
-    // Inject into the webpage
-    document.body.insertBefore(this.hostNode, document.body.firstChild);
+    await this.injectStyles();
+    await this.injectMarkup();
+    document.body.insertBefore(this.hostNode, document.body.firstChild); // Inject webpage's DOM
   }
 
   async injectStyles() {
@@ -29,9 +31,11 @@ export default class ShadowDOMContainer {
     this.shadowRoot.appendChild(styles);
   }
 
-  injectMarkup() {
-    this.wrapper = document.createElement('div');
-    this.wrapper.setAttribute('id', 'image-sidebar')
-    this.shadowRoot.appendChild(this.wrapper);
+  async injectMarkup() {
+    const parser = new DOMParser();
+    const markupStr = await fetchResourceString('html/sidebar.html');
+    const markup = parser.parseFromString(markupStr, 'text/html');
+    this.container = markup.body.firstElementChild; // Keep reference of the container
+    this.shadowRoot.appendChild(this.container);
   }
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import ShadowDOMContainer from './ShadowDOMContainer';
+import { promisifyImageLoad, updateLabel } from '../utility/helper';
 
 const SidebarStyles = {
   backgroundColor: 'transparent',
@@ -16,16 +17,73 @@ const SidebarStyles = {
 export default class Sidebar extends ShadowDOMContainer {
   constructor() {
     super(SidebarStyles);
+    this.selectedImages = [];
   }
 
+  loadImagesOntoGallery(images) {
+    const [showcase, loader] = this.container.querySelectorAll('.gallery__showcase, .gallery__loader');
+    let pImages = [];
+
+    // Make each image unique, add listeners to be used to later to shake off broken images
+    for (let index = 0; index < images.length; index++) {
+      const image = images[index];
+      const tileImage = document.createElement('img');
+      tileImage.setAttribute('src', image.src);
+      tileImage.setAttribute('alt', '');
+      tileImage.dataset.index = index;
+      tileImage.classList.add('tile');
+      tileImage.addEventListener('click', this.onImageSelected.bind(this));
+      // Collect list of broken images / successfully loaded ones
+      pImages.push(promisifyImageLoad(tileImage));
+    }
+
+    // Inject the loaded images onto the gallery, dismiss broken images
+    Promise.allSettled(pImages)
+      .then(results => results.filter(({ status }) => status === 'fulfilled'))
+      .then((filtered) => { // TODO: this is for test purposes, remove afterwards
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(filtered)
+          }, 3000);
+        })	
+      })
+      .then(loadedImages => {
+        loader.parentNode.removeChild(loader); // Loader suicides
+      
+        for (const { value: image } of loadedImages) {
+          const layer = document.createElement('div'); // Add inner div as a layer over the image
+          layer.classList.add('tile__layer');
+          layer.appendChild(image);
+          showcase.appendChild(layer); // Tile added to gallery
+        }
+      
+        updateLabel(this.container, 'total-images', loadedImages.length);
+      });
+  }
+
+  onImageSelected(e) {
+    if (e.target.parentElement.classList.contains('tile--selected')) {
+      const { index } = e.target.dataset;
+      this.selectedImages = this.selectedImages.filter(image => image.dataset.index !== index);
+    }
+    else {
+      this.selectedImages.push(e.target);
+    }
+    
+    e.target.parentElement.classList.toggle('tile--selected');
+    updateLabel(this.container, 'selected-images', this.selectedImages.length);
+  }
+
+  // Toggle opening / closing sidebar
+
   openSidebar() {
-    this.wrapper.style.transition = "transform 1s ease-in-out";
-    this.wrapper.style.transform = "translateX(0)";
+    this.container.style.transition = "transform 0.4s ease-in-out";
+    this.container.style.transform = "translateX(0)";
     this.isShown = true;
   }
   
   closeSidebar() {
-    this.wrapper.style.transform = "translateX(100%)";
+    this.container.style.transform = "translateX(100%)";
     this.isShown = false;
   }
 }
