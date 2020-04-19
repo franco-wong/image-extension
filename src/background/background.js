5e14db607982a142a5fe3216814924fed22cc3d7
Naween M <Nmehanmal@hotmail.com>
2020-04-19 16:44:59 -0400

    Deserialize token query parameters into a JS object

--- src/background/background.js
+++ src/background/background.js
@@ -1,7 +1,9 @@
 const now = new Date();
 const accessToken = {
   code: '',
-  expiry: 0
+  expiry: 0,
+  tokenType: '',
+  scope: ''
 };
 
 chrome.browserAction.onClicked.addListener((tab) => {
@@ -27,6 +29,8 @@ function parseAuthResponse(response) {
   const qParams = new URLSearchParams(response); // Does not pick up fragment identifiers, need regex to parse token value
   accessToken.code = response.match(/(?<=#access_token=).*?(?=&)/g)[0];
   accessToken.expiry = parseInt(qParams.get('expires_in'), 10) + Math.round(now.getTime() / 1000);
+  accessToken.tokenType = qParams.get('token_type');
+  accessToken.scope = qParams.get('scope');
 }
 
 function launchWebAuthFlow() {
@@ -49,8 +53,6 @@ function launchWebAuthFlow() {
         if (chrome.runtime.lastError) {
           reject(chrome.runtime.lastError.message);
         }
-        // response is a string, the access token needs to parsed which is in the response string here's an example:
-        // https://hlblienepbdjkedmeiggjhobpaogfjmn.chromiumapp.org/#access_token= ya29.a0Ae4lvC1Odwd_9bO7qjVozxGOoQGc2gT4e2ZtgaRPSecxcDRkTWtPQs--YPYX3gUH_izqgiMINl8uUepf3uNYHSnx2CnsjoKvPMuETqmlNJTeDmMIA9OXvF5LLQCTalkxvNiDr_CKaN9uA98b-aT7NkPrl55MX7R1CsrcrKBy &token_type=Bearer&expires_in=3599&scope=profile%20https://www.googleapis.com/auth/userinfo.profile
         resolve(response);
       }
     );
