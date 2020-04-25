6b95d6b45b5d460d115aff98aed413a2fab7fd82
Franco <wong9661@mylaurier.ca>
2020-04-25 19:30:12 -0400

    Cleaned up the google drive api file

--- src/background/google-drive-apis.js
+++ src/background/google-drive-apis.js
@@ -7,74 +7,77 @@ Content-Length: [NUMBER_OF_BYTES_IN_FILE]
 Authorization: Bearer [YOUR_AUTH_TOKEN]
 */
 var access_token;
+var imageArray = [];
 const BOUNDARY = "naween";
-const DELIMITER = "\r\n--" + BOUNDARY + "\r\n";
-const CLOSE_DELIM = "\r\n--" + BOUNDARY + "--";
+const REQUEST_BODY_DELIM = {
+  DELIMITER: "\r\n--" + BOUNDARY + "\r\n",
+  CLOSE_DELIM: "\r\n--" + BOUNDARY + "--",
+};
 const REQUEST_URI = "https://www.googleapis.com/upload/drive/v3/files";
 const HEADER_CONTENT_TYPE = `multipart/related; boundary=${BOUNDARY}`;
 const UPLOAD_TYPE = "multipart";
-const IMAGE_ARRAYBUFFER = {
-  arrayBuffer: "",
-  length: 0,
-  metadata: {
-    name: "test.png",
-    description: "test test test",
-  },
-};
 
-export function generateRequestBody(arrayBuffer) {
-  var multipartRequestBody =
-    DELIMITER +
-    "Content-Type: application/json; charset=UTF-8\r\n\r\n" +
-    JSON.stringify(IMAGE_ARRAYBUFFER.metadata) +
-    "\r\n" +
-    DELIMITER +
-    "Content-Type: image/png\r\n" +
-    "Content-Transfer-Encoding: base64\r\n\r\n" +
-    arrayBuffer +
-    CLOSE_DELIM;
-  return multipartRequestBody;
-}
-
-export function getFullUploadRequestURI() {
+function getFullUploadRequestURI() {
   return `${REQUEST_URI}?uploadType=${UPLOAD_TYPE}`;
 }
 
-export function getAuthBearer() {
+function getAuthBearer() {
   return `Bearer ${access_token}`;
 }
 
-//TODO: figure out the length of the file
-export function getFileLength() {
-  return 1;
+class ImageClass {
+  constructor(url, contentType, name, description) {
+    this.url = url;
+    this.contentType = contentType;
+    this.base64 = "";
+    this.length = 0;
+    this.metadata = {
+      name,
+      description,
+    };
+  }
 }
 
-export function getBase64OfURL(url) {
+function generateRequestBody(imageObj) {
+  return (
+    REQUEST_BODY_DELIM.DELIMITER +
+    "Content-Type: application/json; charset=UTF-8\r\n\r\n" +
+    JSON.stringify(imageObj.metadata) +
+    "\r\n" +
+    REQUEST_BODY_DELIM.DELIMITER +
+    `Content-Type: ${imageObj.contentType}\r\n` +
+    "Content-Transfer-Encoding: base64\r\n\r\n" +
+    imageObj.base64 +
+    REQUEST_BODY_DELIM.CLOSE_DELIM
+  );
+}
+
+function ArrayBufferToBase64(ab) {
+  return btoa(
+    new Uint8Array(ab).reduce(
+      (data, byte) => data + String.fromCharCode(byte),
+      ""
+    )
+  );
+}
+
+function getBase64OfURL(imageObj) {
   window
-    .fetch(url)
+    .fetch(imageObj.url)
     .then((response) => response.blob())
     .then((blob) => {
       var reader = new FileReader();
       reader.onloadend = function () {
-        console.log(reader.result);
-        IMAGE_ARRAYBUFFER.arrayBuffer = btoa(
-          new Uint8Array(reader.result).reduce(
-            (data, byte) => data + String.fromCharCode(byte),
-            ""
-          )
-        );
-        //String.fromCharCode.apply(null, new Uint8Array(reader.result))
-        IMAGE_ARRAYBUFFER.length = blob.size;
-        // Once the data has been loaded, call the google drive upload
-        pushImages();
+        imageObj.base64 = ArrayBufferToBase64(reader.result);
+        imageObj.length = blob.size;
+        sendRequest(imageObj);
       };
       reader.readAsArrayBuffer(blob);
     });
 }
 
-export function getHeaderJSON() {
-  var requestBody = generateRequestBody(IMAGE_ARRAYBUFFER.arrayBuffer);
-  console.log(requestBody);
+function getHeaderJSON(imageObj) {
+  var requestBody = generateRequestBody(imageObj);
   return {
     method: "POST",
     headers: {
@@ -86,22 +89,32 @@ export function getHeaderJSON() {
   };
 }
 
-export function pushImages() {
-  window.fetch(getFullUploadRequestURI(), getHeaderJSON()).then((response) => {
-    console.log(response);
-  });
+function sendRequest(imageObj) {
+  window
+    .fetch(getFullUploadRequestURI(), getHeaderJSON(imageObj))
+    .then((response) => {
+      console.log(response);
+    });
 }
 
-export function downloadBase64Img() {
-  getBase64OfURL(
-    "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
-    // "https://png.pngtree.com/element_our/png/20180928/beautiful-hologram-water-color-frame-png_119551.jpg"
-  );
-}
-
-export function startUploading(accessToken) {
-  // include a list of images to download
+export function startUploading(accessToken, listOfImages = []) {
   access_token = accessToken;
 
-  downloadBase64Img();
+  var url1 =
+    "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png";
+
+  var url2 =
+    "https://png.pngtree.com/element_our/png/20180928/beautiful-hologram-water-color-frame-png_119551.jpg";
+  var arr = [url1, url2];
+
+  for (let url of arr) {
+    var imageClass = new ImageClass(
+      url,
+      "image/png",
+      "franco's test",
+      "this is a test description"
+    );
+    getBase64OfURL(imageClass);
+    console.log(imageClass);
+  }
 }
