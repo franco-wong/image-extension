36572e798aa07d291c16eb29adc71c0a659783fd
Naween M <Nmehanmal@hotmail.com>
2020-05-26 00:32:40 -0400

    Add tailwindcss to the extension

--- src/content/components/AppModal.vue
+++ src/content/components/AppModal.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app-modal">
+  <div
+    class="bg-gray-200 h-600px fixed top-1/2 left-1/2 z-9000 transform -translate-x-1/2 -translate-y-1/2"
+  >
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <HelloWorld msg="Welcome to Your Vue.js App for web extension" />
+    <h1 class="text-4xl font-bold text-white-500">Hello World!</h1>
   </div>
 </template>
 
@@ -17,6 +20,10 @@ export default {
 </script>
 
 <style>
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+/* 
 #app-modal {
   position: fixed;
   background-color: gray;
@@ -31,5 +38,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-}
+} */
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    inset: {
+      "1/2": "50%",
+    },
+    zIndex: {
+      "9000": 9000,
+    },
+    height: {
+      "600px": "600px",
+    },
+  },
+  variants: {},
+  plugins: [],
+};
