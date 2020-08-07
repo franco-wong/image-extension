8aee4db66e54f426b610e596910aa39d9ffcb650
franco-wong <wong9661@mylaurier.ca>
2020-08-07 10:46:59 -0400

    Remove button components from previous changes (#20)
    * Start implementing logic for button functionality
    
    -created a button component as the basis of the buttons on the app modal
    
    * complete unselecting all images when button is pressed
    
    * Add button upload functionality
    
    -converted imageSelected set to an array of objects
    -removed metadata for now until we know what to include
    
    * Button Styling
    
    * Call unselectAllImages function if the upload was successful
    
    * Create a function for converting the image set to an array for image uploading
    
    * Remove button components and Add normal buttons to AppModal

--- src/content/components/AppModal.vue
+++ src/content/components/AppModal.vue
@@ -13,23 +13,14 @@
       <span>Images found: {{ pageImages }}</span>
       &nbsp;
       <span>Selected images: {{ selectedPageImages }}</span>
-      <AppModalButton
-        :name="uploadToDrive"
-        :purpose="uploadFunction"
-        @function="task"
-      ></AppModalButton>
-      <AppModalButton
-        :name="unselectAll"
-        :purpose="unselectFunction"
-        @function="task"
-      ></AppModalButton>
+      <button @click="unselectFunction()">Unselect Images</button>
+      <button @click="uploadFunction()">Upload Images</button>
     </div>
   </div>
 </template>
 
 <script>
 import ImageGallery from '@components/ImageGallery.vue';
-import AppModalButton from '@components/AppModalButton.vue';
 
 export default {
   name: 'AppModal',
@@ -60,15 +51,10 @@ export default {
     handleClose() {
       this.$store.commit('setShowApp', { status: false });
     },
-    task(purpose) {
-      if(purpose === 'upload'){
-        this.upload();
-      }
-      else if(purpose === 'unselect'){
-        this.$store.commit('setUnselectAllImages');
-      }
+    unselectFunction() {
+      this.$store.commit('setUnselectAllImages');
     },
-    convertImageSetToArray(){
+    convertImageSetToArray() {
       // Convert the selectedImages set to an array of image Objects
       const imagesToUpload = this.$store.state.selectedImages;
       let imagesObj = [];
@@ -81,7 +67,7 @@ export default {
       }
       return imagesObj;
     },
-    upload(){      
+    uploadFunction(){      
       chrome.runtime.sendMessage(
         {
           command: "UPLOAD_IMAGES",
@@ -133,11 +119,12 @@ export default {
 .modal__actions {
   margin-top: auto;
 
-  // button {
-  //   border-width:0;
-  //   background-color:red;
-  //   display: block;
-  //   margin-right: auto;
-  // }
+  button {
+    border-width: 0;
+    padding: 5px;
+    display: inline-block;
+    float: right;
+    margin-left: 3px;
+  }
 }
 </style>
