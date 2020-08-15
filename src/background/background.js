3648fe7f921ba4334793e014e0517bfcd59ceec7
franco-wong <wong9661@mylaurier.ca>
2020-08-15 17:54:44 -0400

    Task/poc dom selector for url (#23)
    * Create regex to determine the search engine and rules to handle page source
    
    * Add the logic to identify a search engine domain
    
    -changed some of the file names to describe the task it handles
    
    * Add logic to navigate DOM from image to the image source
    
    -modified the rules for Google image search
    
    * Add request logic to a secondary yahoo url to get image source

--- src/background/background.js
+++ src/background/background.js
@@ -1,6 +1,8 @@
 import { startUploading } from "./google-drive-apis";
-import { findAndCreateFolder } from "./get-drive-folder";
+import { findAndCreateFolder } from "./RetrieveFolderID";
 import { launchWebAuthFlow, parseAuthResponse } from "./auth";
+import { identifyDomainInURL, requestToSecondaryPageForImageSource } from "../GetDomImageSource/IdentifyDomain";
+import { RULE_SET } from "../GetDomImageSource/DomainRules";
 
 const now = new Date();
 const accessToken = {
@@ -9,7 +11,11 @@ const accessToken = {
   tokenType: "",
   scope: "",
 };
-
+/**
+ *    chrome.tabs.query({active: true, currentWindow: true}, function(tab){
+        console.log(tab.url);
+      });
+ */
 let imagePromisify;
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request);
@@ -18,6 +24,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       console.log("uploading images")
       // find and create a "Image Extension" folder if it doesn't current exist
       findAndCreateFolder(accessToken.code).then((folderId) => {
+        // determines if the domain is a search engine, and will take page_source differently
+        let source_domain = identifyDomainInURL(sender.tab.url);
+        let RULES_OBJ = JSON.parse(RULE_SET["rule_set"]);
+        let result = "N/A";
+
+        if(source_domain in RULES_OBJ) result = RULES_OBJ[source_domain];
+
+        console.log("The rules for",source_domain,"is",result);
+
         imagePromisify = startUploading(
           accessToken.code,
           request.imagesObj,
@@ -35,9 +50,19 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
   return true; // to allow cb to be invoked later on
 });
-
+/**
+ * When the user clicks the browser to bring the image gallary up, it will take the image metadata
+ *  and the page source relative to the image tag (only if the domain is a search engine in the list)
+ *      If there is no relative href, the search engine page link will be used
+ * 
+ * 
+ * 
+ */
 chrome.browserAction.onClicked.addListener((tab) => {
   const epochTime = Math.round(now.getTime() / 1000);
+
+  requestToSecondaryPageForImageSource()
+    .then(result => console.log(result));
 
   if (epochTime < accessToken.expiry) {
     chrome.tabs.sendMessage(tab.id, "");
