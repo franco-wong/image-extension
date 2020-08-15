5f0407de821172622db9a66469351bfef9917414
Naween M <Nmehanmal@hotmail.com>
2020-08-15 17:56:56 -0400

    Update manifest description

--- src/utility/helper.js
+++ src/utility/helper.js
@@ -1,12 +1,3 @@
-class ImgMetaData {
-  constructor(src, alt, width, height) {
-    this.src = src;
-    this.alt = alt;
-    this.width = width;
-    this.height = height;
-  }
-}
-
 export function fetchPageImages() {
   return Array.from(document.querySelectorAll('img'));
 }
